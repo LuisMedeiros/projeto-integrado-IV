709faf1bdac96cad02d6b2501b08d19f7b41e6f9
LuisMedeiros <luis.henriquexy@gmail.com>
2022-11-05 12:30:14 -0300

    Primeiro commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/styles.css">
+</head>
+
+<body>
+    <div class="topo">
+        <h3>Projeto Integrado IV</h3>
+    </div>
+    <hr>
+    <nav class="container">
+        <ul class="example">
+            <li><a href="https://www.linkedin.com/">LinkedIn</a></li>
+            <li><a href="https://www.glassdoor.com.br/index.htm">Glassdoor</a></li>
+            <li><a href="https://br.indeed.com/">Indeed</a></li>
+            <li><a href="https://github.com/">GitHub</a></li>
+        </ul>
+    </nav>
+</body>
+
+</html>
